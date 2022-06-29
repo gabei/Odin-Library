51716c1db844404b1d9ce4cdfb4063e1d7aa5025
gabei <gabe1792@gmail.com>
2022-06-28 20:38:14 -0500

    updated the way in which inputs are collected from the form

--- scripts/script.js
+++ scripts/script.js
@@ -57,40 +57,45 @@ function Book(title, author, description, pages, imageURL, readStatus) {
 /* DOM Interaction Vars
 ___________________________________________*/
 //this must be initialized before data is pulled from storage
-const cardContainer = document.querySelector(".book-container");
+const cardContainer = document.querySelector('.book-container');
 
-const openModal = document.querySelector(".open-modal");
-const closeModal = document.querySelector(".close-modal");
-const modalBackground = document.querySelector(".modal-background");
-const bookModal = document.querySelector(".book-modal");
-const addBook = document.querySelector(".add-book");
-const addBookForm = document.querySelector(".add-book-form__form");
+const openModal = document.querySelector('.open-modal');
+const closeModal = document.querySelector('.close-modal');
+const modalBackground = document.querySelector('.modal-background');
+const bookModal = document.querySelector('.book-modal');
+const addBook = document.querySelector('.add-book');
+const addBookForm = document.querySelector('.add-book-form__form');
 
-openModal.addEventListener("click", showModal);
-closeModal.addEventListener("click", hideModal);
-modalBackground.addEventListener("click", hideModal);
+openModal.addEventListener('click', showModal);
+closeModal.addEventListener('click', hideModal);
+modalBackground.addEventListener('click', hideModal);
 
 /* DOM Interaction Functions
 ___________________________________________*/
 function showModal() {
-  bookModal.classList.add("show-modal");
+  bookModal.classList.add('show-modal');
 }
 
 function hideModal() {
-  bookModal.classList.remove("show-modal");
+  bookModal.classList.remove('show-modal');
 }
 
 /* Add Book Form Functions
 ___________________________________________*/
-addBookForm.addEventListener("submit", function (e) {
-  let inputs = [...addBookForm.elements];
+addBookForm.addEventListener('submit', function (e) {
+  let inputs = [...addBookForm.elements].slice(0, 5);
   let title = inputs[0].value;
   let author = inputs[1].value;
   let description = inputs[2].value;
   let pages = inputs[3].value;
   let imageURL = inputs[4].value;
 
-  let newBook = new Book(title, author, description, pages, imageURL);
+  let formValues = [];
+  inputs.forEach((i) => {
+    formValues.push(i.value);
+  });
+
+  let newBook = new Book(...formValues);
 
   addBookToLibrary(newBook);
   createBookCard(newBook);
@@ -99,44 +104,56 @@ addBookForm.addEventListener("submit", function (e) {
   e.preventDefault();
 });
 
+/* Form Validation
+___________________________________________*/
+
+function validate(element) {
+  if (!element.validity.valid) {
+    element.setCustomValidity('This field does not meet the requirements!');
+    element.reportValidity();
+  } else {
+    element.setCustomValidity('');
+  }
+}
+
 /* Book Cards
 ___________________________________________*/
 function createBookCard(book) {
-  let bookCard = createDOMelement("div", ["book-card"]);
-  let info = createDOMelement("div", ["book-card__info"]);
-  let title = createDOMelement("p", ["book-card__info--title"]);
-  let author = createDOMelement("p", ["book-card__info--author"]);
-  let description = createDOMelement("div", ["book-card__description"]);
-  let buttons = createDOMelement("div", ["book-card__buttons"]);
-  let imageContainer = createDOMelement("div", ["book-card__image"]);
-  let image = createDOMelement("img");
+  let bookCard = createDOMelement('div', ['book-card']);
+  let info = createDOMelement('div', ['book-card__info']);
+  let title = createDOMelement('p', ['book-card__info--title']);
+  let author = createDOMelement('p', ['book-card__info--author']);
+  let description = createDOMelement('div', ['book-card__description']);
+  let buttons = createDOMelement('div', ['book-card__buttons']);
+  let imageContainer = createDOMelement('div', ['book-card__image']);
+  let image = createDOMelement('img');
   let removeButton = createDOMelement(
-    "button",
-    ["button", "button--remove"],
-    "Remove"
+    'button',
+    ['button', 'button--remove'],
+    'Remove'
   );
   let readButton = createDOMelement(
-    "button",
-    ["button", "button--info"],
-    "Mark as Read"
+    'button',
+    ['button', 'button--info'],
+    'Mark as Read'
   );
 
-  removeButton.addEventListener("click", () => {
+  removeButton.addEventListener('click', () => {
     removeBookFromDOM(bookCard);
     removeBookFromLibrary(book);
   });
 
-  readButton.addEventListener("click", () => {
+  readButton.addEventListener('click', () => {
     toggleReadStatus(book);
     book.readStatus === false
-      ? (readButton.innerHTML = "Mark as Read")
-      : (readButton.innerHTML = "Mark as Unread");
+      ? (readButton.innerHTML = 'Mark as Read')
+      : (readButton.innerHTML = 'Mark as Unread');
   });
 
   title.textContent = book.title;
   author.textContent = book.author;
   description.textContent = book.description;
-  image.src = book.imageURL || "../assets/images/missing-cover.jpg";
+  image.src = book.imageURL || '../assets/images/missing-cover.jpg';
 
   imageContainer.append(image);
   buttons.append(readButton, removeButton);
@@ -147,11 +164,11 @@ function createBookCard(book) {
   return;
 }
 
-function createDOMelement(type = "div", classes = [], text = "") {
+function createDOMelement(type = 'div', classes = [], text = '') {
   let newElement = document.createElement(type);
   newElement.classList.add(...classes);
 
-  if (type === "button") newElement.textContent = text;
+  if (type === 'button') newElement.textContent = text;
 
   return newElement;
 }
@@ -171,26 +188,26 @@ function toggleReadStatus(book) {
 // variation on MDN's storage test function
 function storageIsAvailable() {
   try {
-    localStorage.setItem("poweron", "selftest");
-    localStorage.getItem("poweron");
-    localStorage.removeItem("poweron");
+    localStorage.setItem('poweron', 'selftest');
+    localStorage.getItem('poweron');
+    localStorage.removeItem('poweron');
     localStorage.clear();
     return true;
   } catch (error) {
-    console.log("Local Storage is not available");
+    console.log('Local Storage is not available');
     console.error(error);
   }
 }
 
 function initStorage() {
   if (storageIsAvailable) {
-    console.log("Local storage is available.");
+    console.log('Local storage is available.');
     getLocalStorage();
   }
 }
 
 function getLocalStorage() {
-  let books = JSON.parse(localStorage.getItem("myLibrary"));
+  let books = JSON.parse(localStorage.getItem('myLibrary'));
   if (books) {
     addBooksFromStorage(books);
   } else {
@@ -206,7 +223,7 @@ function addBooksFromStorage(books) {
 }
 
 function updateLocalStorage() {
-  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
 }
 
 // Start here.
